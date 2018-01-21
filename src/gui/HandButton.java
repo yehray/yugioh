7aca7528e4b2fda53be428803ede806353894c33
Raymond Yeh <yehray@gmail.com>
2018-01-21 07:06:41 -0800

    Added hand button class

---
+++ src/gui/HandButton.java
@@ -0,0 +1,13 @@
+package gui;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class HandButton extends JButton {
+    public HandButton(ImageIcon i){
+        super(i);
+        this.setPreferredSize(new Dimension(64, 93));
+    }
+
+
+}
