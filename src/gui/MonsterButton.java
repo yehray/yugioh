02c571b23c090cc7eefe0ac28c07bee411924c85
Raymond Yeh <yehray@gmail.com>
2018-01-21 20:11:10 -0800

    Added Mosnter Button

---
+++ src/gui/MonsterButton.java
@@ -0,0 +1,39 @@
+package gui;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+public class MonsterButton extends JButton implements MouseListener {
+
+    public MonsterButton(ImageIcon imageIcon){
+        this.setPreferredSize(new Dimension(100,150));
+
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
+}
