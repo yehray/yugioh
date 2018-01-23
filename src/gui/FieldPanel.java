c7003bb2d1825a85548e93067ab3c8b523f1af46
Raymond Yeh <yehray@gmail.com>
2018-01-22 23:23:47 -0800

    Added methods for summon and set spell

--- src/gui/FieldPanel.java
+++ src/gui/FieldPanel.java
@@ -1,9 +1,34 @@
 package gui;
 
+import game.MonsterCard;
+import game.SpellCard;
+
 import javax.swing.*;
+import java.awt.*;
 
 public class FieldPanel extends JPanel{
-    public FieldPanel(){
+    private JPanel monsterPanel;
+    private JPanel spellPanel;
 
+    public FieldPanel(){
+        JPanel monsterPanel = new JPanel();
+        JPanel spellPanel = new JPanel();
+        this.setLayout(new GridLayout(2,1));
+        this.add(monsterPanel);
+        this.add(spellPanel);
+    }
+
+    public MonsterButton summonMonster(MonsterCard monsterCard){
+        GetCardImage cardImage = new GetCardImage(monsterCard);
+        MonsterButton monsterButton = new MonsterButton(cardImage.getCardImage());
+        monsterPanel.add(monsterButton);
+        return monsterButton;
+    }
+
+    public SpellButton summonMonster(SpellCard spellCard){
+        GetCardImage cardImage = new GetCardImage(spellCard);
+        SpellButton spellButton = new SpellButton(cardImage.getCardImage());
+        spellPanel.add(spellButton);
+        return spellButton;
     }
 }
