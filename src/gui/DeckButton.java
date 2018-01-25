23f459e993a838132c47472a05ae73ff4d63e78e
Raymond Yeh <yehray@gmail.com>
2018-01-24 19:50:24 -0800

    Added deck button

---
+++ src/gui/DeckButton.java
@@ -0,0 +1,40 @@
+package gui;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+public class DeckButton extends JButton implements MouseListener {
+
+    public DeckButton(ImageIcon imageIcon){
+        this.setIcon(imageIcon);
+        this.setPreferredSize(new Dimension(100,150));
+
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
+}
