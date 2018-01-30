ff3fa933ef032d70b4ae958fc28249870aada820
Raymond Yeh <yehray@gmail.com>
2018-01-29 22:57:24 -0800

    updated show larger image listener

--- src/gui/listeners/ShowLargerImage.java
+++ src/gui/listeners/ShowLargerImage.java
@@ -1,4 +1,43 @@
 package gui.listeners;
 
-public class ShowLargerImage {
+import game.Card;
+import gui.GUI;
+import gui.GetCardImage;
+
+import javax.swing.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+public class ShowLargerImage extends MouseAdapter{
+    private JPanel panel;
+    private Card card;
+    private boolean isFirstClick;
+    private JLabel picLabel;
+    private JLabel backLabel;
+
+
+    public ShowLargerImage(JPanel addedPanel, Card addedCard){
+        panel = addedPanel;
+        card = addedCard;
+        isFirstClick = true;
+        picLabel = new JLabel(card.getImageLarge());
+        backLabel = new JLabel(new ImageIcon(this.getClass().getResource("/gui/resources/yugiohCardBackground Large.jpg")));
+
+
+    }
+
+    public void mouseClicked(MouseEvent e) {
+        if(this.isFirstClick){
+            picLabel.setSize(250,375);
+            panel.add(backLabel);
+            this.isFirstClick = false;
+        }
+        else {
+            panel.add(picLabel);
+            backLabel.setSize(250,375);
+            this.isFirstClick = true;
+
+        }
+
+    }
 }
