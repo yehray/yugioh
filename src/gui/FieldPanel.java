814f33eede362fa73b7a755ef8ee0aae23fe51b9
Raymond Yeh <yehray@gmail.com>
2018-01-31 17:40:05 -0800

    updated field panel to show cards

--- src/gui/FieldPanel.java
+++ src/gui/FieldPanel.java
@@ -2,6 +2,7 @@ package gui;
 
 import game.MonsterCard;
 import game.SpellCard;
+import gui.listeners.HighlightArea;
 
 import javax.swing.*;
 import java.awt.*;
@@ -11,24 +12,52 @@ public class FieldPanel extends JPanel{
     private JPanel spellPanel;
 
     public FieldPanel(){
-        JPanel monsterPanel = new JPanel();
-        JPanel spellPanel = new JPanel();
-        this.setLayout(new GridLayout(2,1));
+        this.setOpaque(false);
+        this.setLayout(null);
+
+//        GridLayout zoneLayout = new GridLayout(1,5);
+        OverlapLayout zoneLayout = new OverlapLayout(new Point(145, 0));
+
+
+        monsterPanel = new JPanel();
+        monsterPanel.setBounds(160,10,850,180);
+        monsterPanel.setLayout(zoneLayout);
         this.add(monsterPanel);
+        monsterPanel.setOpaque(false);
+
+
+        spellPanel = new JPanel();
+        spellPanel.setBounds(0,190,850,180);
         this.add(spellPanel);
+        spellPanel.setOpaque(false);
+
+        ImageIcon monsterCardZone = new ImageIcon(this.getClass().getResource("resources/monsterCardZone.jpg"));
+        MonsterButton monsterCardZoneButton = new MonsterButton(monsterCardZone);
+        monsterCardZoneButton.addMouseListener(new HighlightArea(monsterCardZoneButton));
+
+        monsterPanel.add(monsterCardZoneButton);
+        monsterPanel.add(monsterCardZoneButton);
+        monsterPanel.add(monsterCardZoneButton);
+        monsterPanel.add(monsterCardZoneButton);
+        monsterPanel.add(monsterCardZoneButton);
+
+
+
+
+
+
     }
 
     public MonsterButton summonMonster(MonsterCard monsterCard){
-        GetCardImage cardImage = new GetCardImage(monsterCard);
-        MonsterButton monsterButton = new MonsterButton(cardImage.getCardImage());
+        MonsterButton monsterButton = new MonsterButton(monsterCard.getImageSmall());
         monsterPanel.add(monsterButton);
         return monsterButton;
     }
 
-    public SpellButton summonMonster(SpellCard spellCard){
-        GetCardImage cardImage = new GetCardImage(spellCard);
-        SpellButton spellButton = new SpellButton(cardImage.getCardImage());
-        spellPanel.add(spellButton);
-        return spellButton;
-    }
+//    public SpellButton summonMonster(SpellCard spellCard){
+//        GetCardImage cardImage = new GetCardImage(spellCard, "s'");
+//        SpellButton spellButton = new SpellButton(cardImage.getCardImage());
+//        spellPanel.add(spellButton);
+//        return spellButton;
+//    }
 }
