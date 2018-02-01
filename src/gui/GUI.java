a5357e5659389be413bf78d7e458e1f40b0f907b
Raymond Yeh <yehray@gmail.com>
2018-01-31 17:40:49 -0800

    updated positioning of JPanels

--- src/gui/GUI.java
+++ src/gui/GUI.java
@@ -1,36 +1,109 @@
 package gui;
+import javax.imageio.ImageIO;
 import javax.swing.*;
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Cursor;
-import java.awt.Dimension;
-import java.awt.DisplayMode;
-import java.awt.FlowLayout;
-import java.awt.Font;
-import java.awt.GraphicsDevice;
-import java.awt.GraphicsEnvironment;
-import java.awt.GridLayout;
-import java.awt.Point;
-import java.awt.Window;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
 
 public class GUI extends JFrame {
 
-    JLabel background;
-    PlayerPanel activePlayer;
-    PlayerPanel opponent;
+    private JLabel boardBackground;
+    private PlayerPanel activePlayer;
+    private PlayerPanel opponent;
+    private JLabel activeLifePoints;
+    private JLabel opponentLifePoints;
+//    private JPanel cardImagePanel;
 
 
 
-    public GUI(String player1, String player2){
-        setTitle("YU GI OH");
-        background = new JLabel();
-        setSize(1000,750);
 
-        activePlayer = new PlayerPanel(player1);
-        opponent = new PlayerPanel(player2);
+    public GUI(String p1, String p2){
+
+
+        this.setTitle("Yu-Gi-Oh!");
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        boardBackground = new JLabel(new ImageIcon((getClass().getResource("resources/yugiohFieldBackground.jpg"))));
+        setSize(1366,768);
+
+        JPanel cardImagePanel = new JPanel();
+        cardImagePanel.setBounds(890,188,250,375);
+
+        activePlayer = new PlayerPanel(p1, cardImagePanel);
+        boardBackground.add(activePlayer);
+        activePlayer.setBounds(0,400,1366,400);
+
+
+        this.add(activePlayer);
+        this.add(cardImagePanel);
+        this.add(boardBackground);
+
+
+
+
+
+
+
+        //        opponent = new PlayerPanel(p2);
+
+
+//        this.add(opponent, BorderLayout.NORTH);
+
+
+//        HandPanel handPanelP1 = new HandPanel();
+
+//        HandPanel handPanelP2 = new HandPanel();
+
+//        JPanel jPanel = new JPanel();
+//
+
+
+
+        this.setVisible(true);
+
+
+
+
+//        activeLifePoints = new JLabel("Life Points");
+//        activeLifePoints.setFont(new Font("Papyrus", Font.BOLD, 30));
+//        activeLifePoints.setForeground(Color.LIGHT_GRAY);
+//        opponentLifePoints = new JLabel("Life Points");
+//        opponentLifePoints.setFont(new Font("Papyrus", Font.BOLD, 30));
+//        opponentLifePoints.setForeground(Color.LIGHT_GRAY);
+//        background.add(activeLifePoints);
+//        background.add(opponentLifePoints);
+//        activeLifePoints.setSize(150, 100);
+//        activeLifePoints.setLocation(890, 198);
+
+
+
+
+//        JLabel player1 = new JLabel("PLAYER 1 : "+ p1);
+//        player1.setFont(new Font("Papyrus",Font.BOLD,20));
+//        player1.setForeground(Color.LIGHT_GRAY);
+//        JLabel player2 = new JLabel("PLAYER 2 : "+ p2);
+//        player2.setFont(new Font("Papyrus",Font.BOLD,20));
+//        player2.setForeground(Color.LIGHT_GRAY);
+//        player1.setSize(200,30);
+//        player2.setSize(200,30);
+//        player1.setLocation(230, 390);
+//        player2.setLocation(230, 290);
+//        background.add(player1);
+//        background.add(player2);
+
+
     }
 
+    public JLabel getActiveLifePoints() {
+        return activeLifePoints;
+    }
 
+    public JLabel getBoardBackground() {
+        return boardBackground;
+    }
 
+    public PlayerPanel getActivePlayer() {
+        return activePlayer;
+    }
 }
 
--- src/gui/PlayerPanel.java
+++ src/gui/PlayerPanel.java
@@ -5,13 +5,51 @@ import java.awt.*;
 
 public class PlayerPanel extends JPanel{
 
-    public PlayerPanel(String name){
-        this.setOpaque(false);
+    private GUI gui;
+    private JLabel DeckLabel;
+    private JLabel opponentLifePoints;
+    private HandPanel handPanel;
+    private String selectMonsterInHand;
+    private String selectMonsterInField;
+
+
+    public PlayerPanel(String name, JPanel addedPanel){
         this.setName(name);
-        this.setLayout(new BorderLayout());
+        this.setOpaque(false);
+        this.setLayout(null);
+
         FieldPanel fieldPanel = new FieldPanel();
-        HandPanel handPanel = new HandPanel();
-        this.add(fieldPanel, BorderLayout.NORTH);
-        this.add(handPanel, BorderLayout.SOUTH);
+        fieldPanel.setBounds(0,0,850,384);
+        this.add(fieldPanel);
+
+
+        ImageIcon deckBackground = new ImageIcon(this.getClass().getResource("resources/yugiohCardBackground.jpg"));
+        DeckButton deckButton = new DeckButton(deckBackground);
+        deckButton.setBounds(32,10,100,150);
+        this.add(deckButton);
+
+        ImageIcon graveYardBackground = new ImageIcon(this.getClass().getResource("resources/yugiohGraveyardBackground.jpg"));
+        GraveyardButton graveyardButton = new GraveyardButton(graveYardBackground);
+        graveyardButton.setBounds(32,180,100,150);
+        this.add(graveyardButton);
+
+        HandPanel handPanel = new HandPanel(addedPanel);
+        handPanel.setBounds(890,165,315,200);
+        this.add(handPanel);
+
+
+
+
+//        opponentLifePoints = new JLabel("Life Points");
+//        opponentLifePoints.setFont(new Font("Papyrus", Font.BOLD, 30));
+//        opponentLifePoints.setSize(150, 100);
+//        opponentLifePoints.setLocation(100, 100);
+//        this.add(opponentLifePoints);
+
+
+    }
+
+    public HandPanel getHandPanel() {
+        return handPanel;
     }
 }
