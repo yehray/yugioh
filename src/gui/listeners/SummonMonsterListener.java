8024c88c13094540f681db0427960f6297d6fb24
Raymond Yeh <yehray@gmail.com>
2018-02-01 17:55:35 -0800

    added new listeners for monster cards

--- src/gui/listeners/SummonMonsterListener.java
+++ src/gui/listeners/SummonMonsterListener.java
@@ -3,10 +3,20 @@ package gui.listeners;
 import game.MonsterCard;
 import gui.FieldPanel;
 
-public class SummonMonsterListener {
-    public SummonMonsterListener(FieldPanel field, MonsterCard summonedMonster, int position){
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
+public class SummonMonsterListener extends MouseAdapter {
+    private FieldPanel fieldPanel;
+    private MonsterCard monsterCard;
+    public SummonMonsterListener(FieldPanel field, MonsterCard summonedMonster){
+        fieldPanel = field;
+        monsterCard = summonedMonster;
 
+    }
+
+    public void mouseClicked(MouseEvent e) {
+        fieldPanel.summonMonster(monsterCard);
     }
 }
 
