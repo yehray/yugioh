d67d4f6ae110fa6f51aef84349aa51e2baff759b
Raymond Yeh <yehray@gmail.com>
2018-02-06 19:23:32 -0800

    Moved methods to GUI

--- src/gui/FieldPanel.java
+++ src/gui/FieldPanel.java
@@ -10,51 +10,55 @@ import java.awt.*;
 public class FieldPanel extends JPanel{
     private JPanel monsterPanel;
     private JPanel spellPanel;
+    private JPanel cardControlPanel;
+    private PlayerPanel playerPanel;
 
-    public FieldPanel(){
+    public FieldPanel(GUI gui){
+        cardControlPanel = gui.getCardControlPanel();
+        playerPanel = gui.getActivePlayer();
+
         this.setOpaque(false);
         this.setLayout(null);
 
-//        GridLayout zoneLayout = new GridLayout(1,5);
-        OverlapLayout zoneLayout = new OverlapLayout(new Point(145, 0));
+        OverlapLayout cardZoneLayout = new OverlapLayout(new Point(145, 0));
+        OverlapLayout spellZoneLayout = new OverlapLayout(new Point(145, 0));
 
 
         monsterPanel = new JPanel();
         monsterPanel.setBounds(160,10,850,180);
-        monsterPanel.setLayout(zoneLayout);
+        monsterPanel.setLayout(cardZoneLayout);
         this.add(monsterPanel);
         monsterPanel.setOpaque(false);
 
-
         spellPanel = new JPanel();
-        spellPanel.setBounds(0,190,850,180);
+        spellPanel.setBounds(160,180,850,170);
+        spellPanel.setLayout(spellZoneLayout);
         this.add(spellPanel);
         spellPanel.setOpaque(false);
 
         ImageIcon monsterCardZone = new ImageIcon(this.getClass().getResource("resources/monsterCardZone.jpg"));
-        MonsterButton monsterCardZoneButton = new MonsterButton(monsterCardZone);
-        monsterCardZoneButton.addMouseListener(new HighlightArea(monsterCardZoneButton));
-
-        monsterPanel.add(monsterCardZoneButton);
-        monsterPanel.add(monsterCardZoneButton);
-        monsterPanel.add(monsterCardZoneButton);
-        monsterPanel.add(monsterCardZoneButton);
-        monsterPanel.add(monsterCardZoneButton);
+        ImageIcon spellCardZone = new ImageIcon(this.getClass().getResource("resources/spellCardZone.jpg"));
 
 
+        for(int i = 0; i < 5; i++){
+            MonsterButton monsterCardZoneButton = new MonsterButton(monsterCardZone);
+            monsterCardZoneButton.addMouseListener(new HighlightArea(monsterCardZoneButton, cardControlPanel));
+            monsterPanel.add(monsterCardZoneButton);
 
+            SpellButton spellCardZoneButton = new SpellButton(spellCardZone);
+            spellCardZoneButton.addMouseListener(new HighlightArea(spellCardZoneButton, cardControlPanel));
+            spellPanel.add(spellCardZoneButton);
+        }
 
 
 
     }
 
-    public MonsterButton summonMonster(MonsterCard monsterCard){
-        MonsterButton monsterButton = new MonsterButton(monsterCard.getImageSmall());
-        monsterPanel.add(monsterButton);
-        return monsterButton;
+    public JPanel getMonsterPanel() {
+        return monsterPanel;
     }
 
-//    public SpellButton summonMonster(SpellCard spellCard){
+    //    public SpellButton summonMonster(SpellCard spellCard){
 //        GetCardImage cardImage = new GetCardImage(spellCard, "s'");
 //        SpellButton spellButton = new SpellButton(cardImage.getCardImage());
 //        spellPanel.add(spellButton);
--- src/gui/HandPanel.java
+++ src/gui/HandPanel.java
@@ -2,9 +2,8 @@ package gui;
 
 import game.Card;
 import game.MonsterCard;
-import gui.listeners.HighlightArea;
-import gui.listeners.PopUpListener;
-import gui.listeners.ShowLargerImage;
+import game.Player;
+import gui.listeners.*;
 
 
 import javax.swing.*;
@@ -16,40 +15,48 @@ import java.util.ArrayList;
 
 public class HandPanel extends JPanel{
     private ArrayList<HandButton> handButtons;
-    private JPanel handPanel;
+    private JPanel hand;
     OverlapLayout layout;
-    private JPanel panel;
+    private JPanel cardControlPanel;
+    private PlayerPanel playerPanel;
+    private GUI gui;
 
-
-    public HandPanel(JPanel addedPanel){
-        panel = addedPanel;
-        handPanel = new JPanel();
+    public HandPanel(GUI addedGUI){
+        gui = addedGUI;
+        cardControlPanel = gui.getCardControlPanel();
+        playerPanel = gui.getActivePlayer();
+        hand = new JPanel();
         layout = new OverlapLayout(new Point(25, 0));
         layout.setPopupInsets(new Insets(15,10,0,0));
-        handPanel.setLayout(layout);
+        hand.setLayout(layout);
         handButtons = new ArrayList<HandButton>();
         this.setLayout(new FlowLayout());
-        this.add(handPanel);
+        this.add(hand);
         this.setOpaque(false);
 
         MonsterCard summonedSkull = new MonsterCard("Summoned Skull", 5, 2500, 1300);
         MonsterCard blueEyes = new MonsterCard("Blue Eyes White Dragon", 8, 3000, 2500);
 
-        addButton(summonedSkull);
-        addButton(blueEyes);
 
 
     }
 
-    public void addButton(Card card){
-        HandButton addedCard = new HandButton(card.getImageSmall());
-        addedCard.addMouseListener(new HighlightArea(addedCard));
-        addedCard.addMouseListener(new PopUpListener(layout));
-        addedCard.addMouseListener(new ShowLargerImage(panel, card));
-        handPanel.add(addedCard);
+    public void addButton(MonsterCard card){
+        HandButton addedCard = new HandButton(card.getImageSmall(), card);
+//        addedCard.addMouseListener(new SelectHandCardListener(addedCard, gui));
+//        addedCard.addMouseListener(new PopUpListener(layout));
+//        addedCard.addMouseListener(new ShowLargerImage(cardControlPanel, card));
+        hand.add(addedCard);
         addedCard.setVisible(true);
         addedCard.validate();
-        
+
     }
 
+    public OverlapLayout getCurrentLayout() {
+        return layout;
+    }
+
+    public JPanel getHand(){
+        return hand;
+    }
 }
