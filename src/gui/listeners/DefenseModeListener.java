34fd912c21002d7298a0831b3c1e886422f830a6
Raymond Yeh <yehray@gmail.com>
2018-02-26 19:31:53 -0800

    added switch mode exception

--- src/gui/listeners/DefenseModeListener.java
+++ src/gui/listeners/DefenseModeListener.java
@@ -4,6 +4,7 @@ import game.Game;
 import game.MonsterCard;
 import game.Player;
 import gui.*;
+import gui.exceptions.WrongPhaseException;
 
 import javax.swing.*;
 import java.awt.event.MouseAdapter;
@@ -22,33 +23,36 @@ public class DefenseModeListener extends MouseAdapter{
 
 
     public void mouseClicked(MouseEvent e) {
-        MonsterCard monster = gui.getMonsterSelected().getMonsterCard();
-        int index = gui.getMonsterSelected().getIndex();
-        JPanel monsterPanel = gui.getActivePlayer().getFieldPanel().getMonsterPanel();
-        monsterPanel.remove(index);
-
-        if(monster.getMode() == "ATTACK") {
-            game.getCurrentPlayer().switchMonsterMode(monster);
-            RotatedIcon r1 = new RotatedIcon(monster.getImageSmall(), RotatedIcon.Rotate.DOWN);
-            DefenseMonsterButton defenseModeButton = new DefenseMonsterButton(monster.getImageSmall(), monster);
-            defenseModeButton.addMouseListener(new ShowLargerImage(gui.getCardControlPanel(), defenseModeButton.getMonsterCard()));
-            defenseModeButton.addMouseListener(new SelectFieldMonsterListener(defenseModeButton, gui));
-
-            monsterPanel.add(defenseModeButton, index);
-            defenseModeButton.repaint();
-            defenseModeButton.revalidate();
+        if(gui.getGame().getPlayer().getField().getPhase() == "BATTLE PHASE"){
+            JOptionPane.showMessageDialog(null, "Cannot switch modes during battle phase");
+            throw new WrongPhaseException("Cannot switch modes during battle phase");
         }
-        else{
-            game.getCurrentPlayer().switchMonsterMode(monster);
-            MonsterButton monsterButton = new MonsterButton(monster.getImageSmall(), monster);
-            monsterButton.addMouseListener( new ShowLargerImage(gui.getCardControlPanel(), monster));
-            monsterButton.addMouseListener(new SelectFieldMonsterListener(monsterButton, gui));
+        else {
+            MonsterCard monster = gui.getMonsterSelected().getMonsterCard();
+            int index = gui.getMonsterSelected().getIndex();
+            JPanel monsterPanel = gui.getActivePlayer().getFieldPanel().getMonsterPanel();
+            monsterPanel.remove(index);
+            if (monster.getMode() == "ATTACK") {
+                game.getCurrentPlayer().switchMonsterMode(monster);
+                RotatedIcon r1 = new RotatedIcon(monster.getImageSmall(), RotatedIcon.Rotate.DOWN);
+                DefenseMonsterButton defenseModeButton = new DefenseMonsterButton(monster.getImageSmall(), monster);
+                defenseModeButton.addMouseListener(new ShowLargerImage(gui.getCardControlPanel(), defenseModeButton.getMonsterCard()));
+                defenseModeButton.addMouseListener(new SelectFieldMonsterListener(defenseModeButton, gui));
 
-            monsterPanel.add(monsterButton, index);
-            monsterPanel.repaint();
-            monsterPanel.revalidate();
+                monsterPanel.add(defenseModeButton, index);
+                defenseModeButton.repaint();
+                defenseModeButton.revalidate();
+            } else {
+                game.getCurrentPlayer().switchMonsterMode(monster);
+                MonsterButton monsterButton = new MonsterButton(monster.getImageSmall(), monster);
+                monsterButton.addMouseListener(new ShowLargerImage(gui.getCardControlPanel(), monster));
+                monsterButton.addMouseListener(new SelectFieldMonsterListener(monsterButton, gui));
+
+                monsterPanel.add(monsterButton, index);
+                monsterPanel.repaint();
+                monsterPanel.revalidate();
+            }
         }
-
     }
 }
 
