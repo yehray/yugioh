636651fbd279e26e4c05163978f067a2bd2e62d0
Raymond Yeh <yehray@gmail.com>
2018-03-05 18:38:47 -0800

    adding tests

--- test/Tests.java
+++ test/Tests.java
@@ -1,15 +1,27 @@
-import org.junit.Test;
-
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 
 
 public class Tests {
-//Tests    @Test
-//    public void testing() {
-//        int i = 0;
-//        int j = 0;
-//        assertEquals("This should equal", i == j);
-//
-//
-//    }
+
+    @Test
+    public void test1() {
+        int a = 2;
+        assertEquals(1, 1);
+    }
+
+    @Test
+    public void test2() {
+        int a = 2;
+        assertEquals(1, 1);
+    }
+
+    @Test
+    public void test3() {
+        int a = 2;
+        assertEquals(1, 1);
+    }
 }
